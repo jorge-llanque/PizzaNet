0c9d565e405098d6959d9dcd790185de570c43e0
jorge <georgellanque@gmail.com>
2021-12-27 11:20:38 -0400

    Agregar archivos de proyecto.

---
+++ PizzaNet.Model/Entities/Product.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PizzaNet.Model.Entities
+{
+    class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Size { get; set; }
+        public string Capacity { get; set; }
+        public decimal Price { get; set; }
+    }
+}
---
+++ PizzaNet.Model/Entities/Stock.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PizzaNet.Model.Entities
+{
+    class Stock
+    {
+        public int Id { get; set; }
+        public int MinLimit { get; set; }
+        public int Cantity { get; set; }
+        public DateTime DueDate { get; set; }
+        public Product productId { get; set; }
+
+    }
+}
